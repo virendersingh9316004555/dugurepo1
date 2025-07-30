586282a395b0b7474d3c7793341a3cf54967a174
virendersingh9316004555 <virendersingh9316004555@gmail.com>
2025-07-30 21:17:46 +0530

    updated code

--- todo_infra/main.tf
+++ todo_infra/main.tf
@@ -3,6 +3,7 @@ module "resource_group" {
   resource_group_name     = "rg_duggu"
   resource_group_location = "West US"
 }
+# bhai kahani likh de 
 
 module "resource_group1" {
   source                  = "../modules/azurerm_resource_group"
